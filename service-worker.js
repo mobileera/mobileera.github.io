64ae575411dfb7e4ef6383f5b8c7c2b405eefb5f
Hampus Joakim Nilsson <mail@hjnilsson.com>
2016-10-03 17:43:58 +0200

    Built zeppelin-grunt from commit 08aeee4 on branch source

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["css/main.css","1d378f313daa7f30bdc393f02486d535"],["googlee83bd89684c78156.html","b814d2c9d96f8432e4685f5b513f8088"],["index.html","c50bc4b54ed67898f20b90fdfd8c41df"],["js/bootstrap.min.js","b6d6c550cb657155feb06fdcd34b73cb"],["js/jquery.min.js","33cabfa15c1060aaa3d207c653afb1ee"],["js/libraries.js","23b4082d6fcb6cbced4d3443941a32cb"],["js/scripts.js","d35f0c60a2cdb3db4867b78c7e2ebe79"],["speaker.html","8a29371e0f42e30313241fa6cce8e2d1"],["sponsor.html","f603520613cbbb7da27da090154099ff"]];
+var precacheConfig = [["css/main.css","1d378f313daa7f30bdc393f02486d535"],["googlee83bd89684c78156.html","b814d2c9d96f8432e4685f5b513f8088"],["index.html","c50bc4b54ed67898f20b90fdfd8c41df"],["js/bootstrap.min.js","b6d6c550cb657155feb06fdcd34b73cb"],["js/jquery.min.js","d0212568ce69457081dacf84e327fa5c"],["js/libraries.js","23b4082d6fcb6cbced4d3443941a32cb"],["js/scripts.js","d35f0c60a2cdb3db4867b78c7e2ebe79"],["speaker.html","8a29371e0f42e30313241fa6cce8e2d1"],["sponsor.html","f603520613cbbb7da27da090154099ff"]];
 var cacheName = 'sw-precache-v2-zeppelin-grunt-' + (self.registration ? self.registration.scope : '');
 
 
