8ce842f3d6725c745db055551b1f12cab92b4cf0
Maxim Salnikov <salnikov@gmail.com>
2017-03-18 18:06:25 +0100

    cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,61 +3,15 @@
 
 var packageJson = require('./package.json');
 var path = require('path');
-var swPrecache = require('./node_modules/sw-precache/lib/sw-precache.js');
-
 
 module.exports = function(grunt) {
 
-    function writeServiceWorkerFile(rootDir, handleFetch, callback) {
-        var config = {
-            cacheId: packageJson.name,
-            dynamicUrlToDependencies: {},
-            // If handleFetch is false (i.e. because this is called from swPrecache:dev), then
-            // the service worker will precache resources but won't actually serve them.
-            // This allows you to test precaching behavior without worry about the cache preventing your
-            // local changes from being picked up during the development cycle.
-            handleFetch: handleFetch,
-            logger: grunt.log.writeln,
-            staticFileGlobs: [
-                rootDir + '/css/**.css',
-                rootDir + '/**.html',
-                rootDir + '/img/**.*',
-                rootDir + '/js/**.js'
-            ],
-            stripPrefix: rootDir + '/',
-            // verbose defaults to false, but for the purposes of this demo, log more.
-            verbose: true
-        };
-
-        swPrecache.write(path.join(rootDir, 'service-worker.js'), config, callback);
-    }
-
-    grunt.registerMultiTask('swPrecache', function() {
-        var done = this.async();
-        var rootDir = this.data.rootDir;
-        var handleFetch = this.data.handleFetch;
-
-        writeServiceWorkerFile(rootDir, handleFetch, function(error) {
-            if (error) {
-                grunt.fail.warn(error);
-            }
-            done();
-        });
-    });
-
     require('time-grunt')(grunt);
     require('jit-grunt')(grunt, {
-        buildcontrol: 'grunt-build-control',
-        swPrecache: 'swPrecache'
+        buildcontrol: 'grunt-build-control'
     });
 
     grunt.initConfig({
-        swPrecache: {
-            dev: {
-                handleFetch: false,
-                rootDir: 'dist'
-            }
-        },
         app: {
             source: 'app',
             dist: 'dist',
@@ -374,7 +328,7 @@ module.exports = function(grunt) {
                 }, {
                     expand: true,
                     cwd: 'app/_static',
-                    src: ['*'],
+                    src: ['**/*'],
                     dest: '<%= app.dist %>/<%= app.baseurl %>'
                 }]
             }
@@ -427,8 +381,7 @@ module.exports = function(grunt) {
         'cssmin',
         'critical',
         'copy:dist',
-        'htmlmin',
-        'swPrecache'
+        'htmlmin'
     ]);
 
     grunt.registerTask('deploy', [
